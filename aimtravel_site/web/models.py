7ddf7e614118b6a2a5521c17d224c8d935220b15
Zahariev <vzahariev@hpe.com>
2023-09-07 01:44:00 +0300

    updated models, index and offer pages 07.09.2023

--- aimtravel_site/web/models.py
+++ aimtravel_site/web/models.py
@@ -13,6 +13,58 @@ class City(models.Model):
         verbose_name_plural = 'Градове'
 
     CITY = 20
+    STATES = (
+        ('AL', 'AL'),
+        ('AK', 'AK'),
+        ('AZ', 'AZ'),
+        ('AR', 'AR'),
+        ('CA', 'CA'),
+        ('CO', 'CO'),
+        ('CT', 'CT'),
+        ('DE', 'DE'),
+        ('FL', 'FL'),
+        ('GA', 'GA'),
+        ('HI', 'HI'),
+        ('ID', 'ID'),
+        ('IL', 'IL'),
+        ('IN', 'IN'),
+        ('IA', 'IA'),
+        ('KS', 'KS'),
+        ('KY', 'KY'),
+        ('LA', 'LA'),
+        ('ME', 'ME'),
+        ('MD', 'MD'),
+        ('MA', 'MA'),
+        ('MI', 'MI'),
+        ('MN', 'MN'),
+        ('MS', 'MS'),
+        ('MO', 'MO'),
+        ('MT', 'MT'),
+        ('NE', 'NE'),
+        ('NV', 'NV'),
+        ('NH', 'NH'),
+        ('NJ', 'NJ'),
+        ('NM', 'NM'),
+        ('NY', 'NY'),
+        ('NC', 'NC'),
+        ('ND', 'ND'),
+        ('OH', 'OH'),
+        ('OK', 'OK'),
+        ('OR', 'OR'),
+        ('PA', 'PA'),
+        ('RI', 'RI'),
+        ('SC', 'SC'),
+        ('SD', 'SD'),
+        ('TN', 'TN'),
+        ('TX', 'TX'),
+        ('UT', 'UT'),
+        ('VT', 'VT'),
+        ('VA', 'VA'),
+        ('WA', 'WA'),
+        ('WV', 'WV'),
+        ('WI', 'WI'),
+        ('WY', 'WY'),
+    )
 
     name = models.CharField(
         unique=True,
@@ -21,6 +73,14 @@ class City(models.Model):
         blank=True,
         null=True,
     )
+    state = models.CharField(
+        choices=STATES,
+        verbose_name='Щат',
+        max_length=CITY,
+        blank=True,
+        null=True,
+    )
+
     fact1 = models.TextField(
 
         verbose_name='Факт 1',
@@ -163,12 +223,6 @@ class JobOffer(models.Model):
         City,
         on_delete=models.CASCADE,
         to_field='name'
-    )
-    state = models.CharField(
-        verbose_name='Щат',
-        max_length=STATE_NAME,
-        blank=True,
-        null=True,
     )
     job_position = models.CharField(
         verbose_name='Работна позиция',
