ef44b7a7ce75fea137bc912433b22e11d56b91b8
Zahariev <vzahariev@hpe.com>
2023-09-21 00:06:31 +0300

    scrollbar to offer filters added

--- static/javascript/js_filtering.js
+++ static/javascript/js_filtering.js
@@ -12,3 +12,22 @@ $(document).ready(function() {
             $('#job-offer-filter-form').submit();
         });
     });
+
+
+$(document).ready(function scrollBarChecker() {
+  var dropdown = document.querySelector(".checkbox-row");
+
+  // Function to check if the scrollbar is needed and show/hide it accordingly
+  function checkScrollbar() {
+    var isScrollbarNeeded = dropdown.scrollHeight > dropdown.clientHeight;
+    if (isScrollbarNeeded) {
+      dropdown.style.paddingRight = "20px"; // Add space for scrollbar
+    } else {
+      dropdown.style.paddingRight = "0"; // Remove space if scrollbar is not needed
+    }
+  }
+
+  // Check scrollbar initially and when the window is resized
+  checkScrollbar();
+  $(window).on("resize", checkScrollbar);
+});
