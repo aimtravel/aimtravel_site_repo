0923889a1868f68738d8df0c9b4a4e9e8225ddcb
Zahariev <vzahariev@hpe.com>
2023-09-24 23:49:04 +0300

    not important update

--- aimtravel_site/web/urls.py
+++ aimtravel_site/web/urls.py
@@ -17,9 +17,9 @@ urlpatterns = (
     # ])),
     path('taxes/', taxes, name='taxes'),
     path('onlineservices/', online, name='online'),
+    path('rabotnioferti/', JobOfferListView.as_view(), name='offers'),
     path('offer/', include([
         path('add/', CreateOfferView.as_view(), name='add offer'),
-        path('all/', JobOfferListView.as_view(), name='offers'),
         path('edit/<int:pk>/', EditOfferView.as_view(), name='edit offer'),
         path('delete/<int:pk>/', DeleteOfferView.as_view(), name='delete offer'),
         path('details/<int:pk>/', DetailsOfferView.as_view(), name='details offer'),
