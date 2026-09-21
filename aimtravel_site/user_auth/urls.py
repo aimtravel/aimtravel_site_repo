6a9ca85f71a3b955da1e7b59ab54da75de7e7e37
Peshako <p.stoyanov@aimtravel.bg>
2026-09-21 23:23:07 +0300

    Build unified student and consultant login

--- aimtravel_site/user_auth/urls.py
+++ aimtravel_site/user_auth/urls.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.views import PasswordChangeView
+from django.views.generic import RedirectView
 from django.urls import path
 from aimtravel_site.user_auth.views import *
 
 
 urlpatterns = (
     path('', SignInView.as_view(), name='sign in'),
-    path('register/', SignUpView.as_view(), name='sign up'),
+    path('register/', RedirectView.as_view(pattern_name='sign in'), name='sign up'),
+    path('portal/', StudentPortalView.as_view(), name='student portal'),
     path('sign-out/', SignOutView.as_view(), name='sign out'),
     path('change_password/',
          PasswordChangeView.as_view(template_name='user_auth/change-password.html', success_url='/'),
