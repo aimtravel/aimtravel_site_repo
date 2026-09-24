9a9a985f3d7955f0b3ee63535ec486e6620312d6
Peshako <p.stoyanov@aimtravel.bg>
2026-09-24 23:17:13 +0300

    Route favorite inquiry form

--- aimtravel_site/web/urls.py
+++ aimtravel_site/web/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
 from aimtravel_site.web.views import *
-from aimtravel_site.web.favorites import favorites_page, remove_favorite, export_favorites
+from aimtravel_site.web.favorites import favorites_page, remove_favorite, favorite_inquiry, export_favorites
 
 urlpatterns = (
     path('rabotnioferti/lyubimi/', favorites_page, name='favorite offers'),
     path('rabotnioferti/lyubimi/remove/', remove_favorite, name='remove favorite'),
+    path('rabotnioferti/lyubimi/zapitvane/', favorite_inquiry, name='favorite inquiry'),
     path('crm/offers/export/', export_favorites, name='export favorites'),
     # path('', CombinedView.as_view(), name='index'),
     path('under-construction/', under_construction, name='under-construction'),
